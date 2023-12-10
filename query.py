4a0d9f82b42b351dcd8b396ee739fb8c6fcc48b9
Ubuntu <ubuntu@ip-172-31-37-18.ap-southeast-1.compute.internal>
2023-12-10 14:00:05 +0700

    Added gunicorn to deploy flask app on dev

--- query.py
+++ query.py
@@ -1,19 +1,8 @@
-import argparse
-from dataclasses import dataclass
-from langchain.vectorstores.chroma import  Chroma
-from langchain.embeddings import OpenAIEmbeddings
-from langchain.chat_models import ChatOpenAI
-from langchain.prompts import ChatPromptTemplate, PromptTemplate, HumanMessagePromptTemplate, MessagesPlaceholder, SystemMessagePromptTemplate
-from langchain.chains import ConversationalRetrievalChain, LLMChain
+from langchain.chains import LLMChain
 from langchain.memory import ConversationBufferMemory
 from langchain.chains.conversational_retrieval.base import BaseConversationalRetrievalChain
-from langchain.tools.render import  render_text_description
 from langchain.llms import OpenAI
-from langchain.agents.output_parsers import ReActSingleInputOutputParser
-from langchain.agents.format_scratchpad import format_log_to_str
-from langchain.agents import tool, AgentExecutor, load_tools, ZeroShotAgent
-from langchain.tools import Tool
-from langchain.chains import LLMMathChain
+from langchain.agents import AgentExecutor, load_tools, ZeroShotAgent
 
 from dotenv import load_dotenv
 from PdfKnowledge import *
